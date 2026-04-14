3560ce66644c42a018e96a28a0f5b0766cea457f
Hanvalad <hanvaladbabaev@yandex.ru>
2026-04-14 21:47:11 +0300

    fix: исправлены ошибки тестов

--- ewm-stats-service/stats-dto/src/main/java/ru/practicum/dto/ViewStatsDto.java
+++ ewm-stats-service/stats-dto/src/main/java/ru/practicum/dto/ViewStatsDto.java
@@ -1,40 +1,15 @@
 package ru.practicum.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ViewStatsDto {
+
     private String app;
     private String uri;
     private Long hits;
-
-    public ViewStatsDto() {
-    }
-
-    public ViewStatsDto(String app, String uri, Long hits) {
-        this.app = app;
-        this.uri = uri;
-        this.hits = hits;
-    }
-
-    public String getApp() {
-        return app;
-    }
-
-    public void setApp(String app) {
-        this.app = app;
-    }
-
-    public String getUri() {
-        return uri;
-    }
-
-    public void setUri(String uri) {
-        this.uri = uri;
-    }
-
-    public Long getHits() {
-        return hits;
-    }
-
-    public void setHits(Long hits) {
-        this.hits = hits;
-    }
 }
